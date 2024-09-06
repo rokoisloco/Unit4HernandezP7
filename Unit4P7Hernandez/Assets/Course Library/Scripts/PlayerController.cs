533d20bcd570d7529dc4b49beed3e5423c29bbb7
Byron Hernandez <145072285+rokoisloco@users.noreply.github.com>
2024-09-06 15:20:08 -0700

    Finished
    Game is completely finished.

--- Unit4P7Hernandez/Assets/Course Library/Scripts/PlayerController.cs
+++ Unit4P7Hernandez/Assets/Course Library/Scripts/PlayerController.cs
@@ -18,6 +18,13 @@ public class PlayerController : MonoBehaviour
     private GameObject tmpRocket; 
     private Coroutine powerupCountdown;
 
+    public float hangTime;
+    public float smashSpeed;
+    public float explosionForce;
+    public float explosionRadius;
+
+    bool smashing = false;
+    float floorY;
     // Start is called before the first frame update
     void Start()
     {
@@ -37,6 +44,11 @@ public class PlayerController : MonoBehaviour
             LaunchRockets();
         }
 
+        if (currentPowerUp == PowerUpType.Smash && Input.GetKeyDown(KeyCode.Space) && !smashing)
+        {
+            smashing = true; StartCoroutine(Smash()); 
+        }
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -44,7 +56,7 @@ public class PlayerController : MonoBehaviour
         if (other.CompareTag("Powerup"))
         {
             hasPowerup = true;
-            currentPowerUp = other.gameObject.GetComponent<PowerUpType>();
+            currentPowerUp = other.gameObject.GetComponent<PowerUp>().powerUpType;
             Destroy(other.gameObject);
             StartCoroutine(PowerupCountdownRoutine());
             powerupIndicator.gameObject.SetActive(true);
@@ -87,6 +99,33 @@ public class PlayerController : MonoBehaviour
         hasPowerup = false;
         currentPowerUp = PowerUpType.None;
         powerupIndicator.gameObject.SetActive(false);
+    }
+
+    IEnumerator Smash()
+    {
+        var enemies = FindObjectsOfType<Enemy>();
+
+        floorY = transform.position.y;
+
+        float jumpTime = Time.time + hangTime;
+
+        while(Time.time < jumpTime)
+        {
+            playerRb.velocity = new Vector2(playerRb.velocity.x, smashSpeed);
+            yield return null;
+        }
+
+        while(transform.position.y > floorY)
+        {
+            playerRb.velocity = new Vector2(playerRb.velocity.x, -smashSpeed * 2);
+            yield return null;
+        }
+
+        for (int i = 0; i < enemies.Length; i++) 
+        {
+            if (enemies[i] != null) enemies[i].GetComponent<Rigidbody>().AddExplosionForce(explosionForce, transform.position, explosionRadius, 0.0f, ForceMode.Impulse);
+        }
+
     }
 
 }
